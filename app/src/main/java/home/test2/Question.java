24f1dcd301694d058b9f1e76fb86c2469c27a705
Christal Spaeth <caspaeth@uwm.edu>
2016-03-17 14:07:08 -0500

    Added template object structure for question class

---
+++ app/src/main/java/home/test2/Question.java
@@ -0,0 +1,41 @@
+package home.test2;
+
+/**
+ * Created by christal on 3/17/2016.
+ */
+public class Question {
+    private String subject;
+    private String section;
+    private String problem;
+    private String answer;
+
+    public void setSubject(String subject){
+        this.subject = subject;
+    }
+    public String getSubject(){
+        return subject;
+    }
+
+    public void setSection(String section){
+        this.section = section;
+    }
+    public String getSection(String section){
+        return section;
+    }
+
+    public void setProblem(String problem){
+        this.section = problem;
+    }
+    public String getProblem(String problem){
+        return problem;
+    }
+
+    public void setAnswer(String answer){
+        this.section = section;
+    }
+    public String getAnswer(String answer){
+        return answer;
+    }
+
+
+}
