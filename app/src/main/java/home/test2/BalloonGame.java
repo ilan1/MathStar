42fa1c6ca15f09acd1cb22a100025532257c11cf
Christal Spaeth <caspaeth@uwm.edu>
2016-03-29 19:32:28 -0500

    added background music to game screen

--- app/src/main/java/home/test2/BalloonGame.java
+++ app/src/main/java/home/test2/BalloonGame.java
@@ -1,15 +1,14 @@
 package home.test2;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.media.MediaPlayer;
 
 public class BalloonGame extends AppCompatActivity {
+
+    MediaPlayer backgroundtheme;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +19,17 @@ public class BalloonGame extends AppCompatActivity {
 
         //setContentView(R.layout.activity_balloon_game);
         setContentView(new BalloonGameView(this));
+
+        backgroundtheme = MediaPlayer.create(BalloonGame.this, R.raw.balloon_blast);
+        backgroundtheme.setLooping(true);
+        backgroundtheme.start();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        backgroundtheme.release();
+        finish();
     }
 
 }
