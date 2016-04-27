b9403233748faf63d6efccfd8cf2de91d55ec469
Aleksandar Styles <aleksandarstyles@gmail.com>
2016-04-26 20:41:30 -0500

    Timer game work, fixed algebraic functions on lesson menu not going to the timer game

--- app/src/main/java/home/test2/LessonMenu.java
+++ app/src/main/java/home/test2/LessonMenu.java
@@ -56,7 +56,7 @@ public class LessonMenu extends AppCompatActivity {
                     startActivity(i);
                   //  Toast.makeText(getApplicationContext(), "You must select at least 1 chapter.", Toast.LENGTH_LONG).show();
                 }
-                else if(sections.contains("Linear Equations") || sections.contains("Right Triangles and Trigonometry")){
+                else if(sections.contains("Algebraic Functions") || sections.contains("Right Triangles and Trigonometry")){
                     i = new Intent(LessonMenu.this, TimerGame.class);
                     startActivity(i);
                 }
--- app/src/main/java/home/test2/TimerGame.java
+++ app/src/main/java/home/test2/TimerGame.java
@@ -26,14 +26,7 @@ public class TimerGame extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
 
         timer = new Timer();
         interval = secs;
