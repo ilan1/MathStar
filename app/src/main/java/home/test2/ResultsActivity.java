416aa196c52bb2f19b5eb20b8cc64ad32f405b83
ilan1 <ilan@uwm.edu>
2016-04-26 20:45:22 -0500

    Updated results design and added pop-up
    Added a pop up for each result so the user can see the question.

--- app/src/main/java/home/test2/ResultsActivity.java
+++ app/src/main/java/home/test2/ResultsActivity.java
@@ -1,14 +1,17 @@
 package home.test2;
 
 import android.app.ActionBar;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -20,6 +23,9 @@ import java.util.List;
 
 public class ResultsActivity extends AppCompatActivity {
 
+    private ArrayList<Question> questions;
+    private ArrayList<String> responses;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,8 +35,8 @@ public class ResultsActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         Intent intent = getIntent();
-        ArrayList<Question> questions = (ArrayList<Question>) intent.getSerializableExtra("questions");
-        ArrayList<String> responses = intent.getStringArrayListExtra("responses");
+        questions = (ArrayList<Question>) intent.getSerializableExtra("questions");
+        responses = intent.getStringArrayListExtra("responses");
         TableLayout resultsTableLayout = (TableLayout) findViewById(R.id.resultsTableLayout);
 
         for(int i = 0; i < 10; ++i){
@@ -49,23 +55,38 @@ public class ResultsActivity extends AppCompatActivity {
                 textView.setTextColor(Color.parseColor("#AA3333"));
             }
 
-            s += " | ";
+            s += " \n\n ";
             s += "Response = " + responses.get(i);
             s += " | ";
             s += "Answer = " + questions.get(i).getAnswer();
             textView.setText(s);
 
             textView.setPadding(20, 20, 20, 20);
-            textView.setBackgroundColor(Color.parseColor("#EEEEEE"));
+            textView.setBackgroundColor(Color.parseColor("#FFEEEEEE"));
             textView.setWidth(this.getWindowManager().getDefaultDisplay().getWidth());
             shadow.addView(textView);
 
             shadow.setBottom(20);
             shadow.setPadding(0, 0, 3, 3);
-            shadow.setBackgroundColor(Color.parseColor("#55888888"));
+            shadow.setBackgroundColor(Color.parseColor("#FF888888"));
             row.setPadding(0,0,0,40);
             row.addView(shadow);
 
+            final String problem = questions.get(i).getProblem().toString();
+            row.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    final AlertDialog alertDialog;
+                    alertDialog = new AlertDialog.Builder(v.getContext()).create();
+                    alertDialog.setTitle("Question");
+                    alertDialog.setMessage(problem);
+                    alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                        }
+                    });
+                    alertDialog.show();
+                }
+            });
             resultsTableLayout.addView(row);
         }
 
