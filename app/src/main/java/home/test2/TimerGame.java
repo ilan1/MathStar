b661e3b9d945c6b6ddbe43fc44b0a0c6a622bcae
hzygone <hzy_@live.com>
2016-04-26 19:07:18 -0500

    Timer Game start

--- app/src/main/java/home/test2/TimerGame.java
+++ app/src/main/java/home/test2/TimerGame.java
@@ -6,8 +6,18 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class TimerGame extends AppCompatActivity {
+
+    TextView timerview;
+     Timer timer;
+    int interval;
+    int secs = 1000;
+    int score = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +34,22 @@ public class TimerGame extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        timer = new Timer();
+        interval = secs;
+        timer.scheduleAtFixedRate(new TimerTask() {
+            int i = interval;
+            public void run() {
+                System.out.println(i--);
+                if (i< 0)
+                {
+                    timerview.setText("Sorry your time is up and your score is: " +  score);
+                    timer.cancel();
+
+
+                }
+            }
+        }, 0, 1000);
     }
 
 }
